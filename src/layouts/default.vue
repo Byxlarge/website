6ac3d42dd0e32f8b82be1e7d6e699fd89447c90a
Mehmetali345Dev <345m.ali2007@gmail.com>
2021-12-05 20:53:58 +0300

    blursed version with background image

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,26 +1,66 @@
 <template>
   <div
-    class="flex min-h-screen flex-col items-center bg-gray-200 scrollbar text-black dark:(bg-black text-gray-50)"
+    class="flex relative min-h-screen flex-col items-center bg-gray-200 scrollbar text-black dark:(bg-black text-gray-50)"
   >
-    <transition name="fade" mode="out-in">
-      <NavDefault class="sticky top-0 z-40" />
-    </transition>
-
     <div
-      class="flex flex-col w-11/12 md:w-9/12 my-6 z-10 h-full flex-grow items-start justify-center"
+      class="md:flex hidden focus:outline-none"
+      :class="{
+        'fixed w-full h-full': itemLoaded === false,
+      }"
+      :style="{
+        backgroundImage: itemLoaded === true ? `url('${image}')` : '',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }"
     >
-      <Nuxt class="flex-grow" />
+      <img
+        :src="image"
+        class="invisible"
+        draggable="false"
+        alt="image"
+        width="100%"
+        height="100%"
+        @load="itemLoaded = true"
+      />
     </div>
-    <NavFooter
-      class="dark:(bg-gray-900 bg-opacity-30) bg-gray-300 bg-opacity-30"
-      v-if="$route.path.includes('blog')"
-    />
-    <div class="bg-green-600 h-2 w-full" />
+    <div
+      class="w-full absolute z-20 flex flex-col items-center h-full"
+      :class="{ 'text-white': itemLoaded }"
+    >
+      <transition name="fade" mode="out-in">
+        <NavDefault class="sticky top-0 z-40" />
+      </transition>
+
+      <div
+        class="flex flex-col w-11/12 md:w-9/12 my-6 z-10 h-full flex-grow items-start justify-center"
+      >
+        <Nuxt class="flex-grow" />
+      </div>
+      <NavFooter
+        class="dark:(bg-gray-900 bg-opacity-30) bg-gray-300 bg-opacity-30"
+        v-if="$route.path.includes('blog')"
+      />
+      <div class="bg-green-600 h-2 w-full" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      image: '',
+      itemLoaded: false,
+    }
+  },
+  async fetch() {
+    const { data: img } = await this.$axios.get(
+      'https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN'
+    )
+    console.log(img)
+    this.image = img.url
+  },
+}
 </script>
 
 <style></style>
